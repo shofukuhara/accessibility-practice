77f498ad49992c3ad90a3be8130be0bc00ee4159
shofukuhara <sho.fukuhara0213@gmail.com>
2025-07-13 00:03:20 +0900

    wip:一旦コミット

--- src/assets/js/ui/accordion.js
+++ src/assets/js/ui/accordion.js
@@ -2,42 +2,58 @@ import { gsap } from "gsap";
 
 export class Accordion {
   constructor() {
-    this.ngItem = document.querySelector("[data-ng-accordion]");
-    this.ngText = document.querySelector("[data-ng-text]");
+    this.ok = {
+      trigger: document.querySelector("[data-ok-trigger]"),
+      item: document.querySelector("[data-ok-accordion]"),
+      text: document.querySelector("[data-ok-text]"),
+      icon: document.querySelector("[data-ok-icon]"),
+    };
+
+    this.IS_OPENED_CLASS = "is-opened";
   }
 
   init() {
-    this.ngItem.addEventListener("click", () => {
-      if (this.ngItem.getAttribute("data-ng-accordion") === "closed") {
-        this._open();
+    const { trigger, item, text, icon } = this.ok;
+
+    trigger.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const isOpened = item.classList.contains(this.IS_OPENED_CLASS);
+
+      if (isOpened) {
+        item.classList.remove(this.IS_OPENED_CLASS);
+        this._close(item, text);
+        gsap.set(icon, { rotate: 0 });
       } else {
-        this._close();
+        item.classList.add(this.IS_OPENED_CLASS);
+        item.setAttribute("open", "true");
+        this._open(text);
+        gsap.set(icon, { rotate: -180 });
       }
     });
   }
 
-  _open() {
+  _open(content) {
     gsap.fromTo(
-      this.ngText,
+      content,
       { height: 0 },
       {
         height: "auto",
-        duration: 0.5,
-        ease: "power1.out",
-        onComplete: () => {
-          this.ngItem.setAttribute("data-ng-accordion", "open");
-        },
+        duration: 0.4,
+        ease: "power3.out",
+        overwrite: true,
       }
     );
   }
 
-  _close() {
-    gsap.to(this.ngText, {
+  _close(item, content) {
+    gsap.to(content, {
       height: 0,
-      duration: 0.5,
-      ease: "power1.out",
+      duration: 0.4,
+      ease: "power3.out",
+      overwrite: true,
       onComplete: () => {
-        this.ngItem.setAttribute("data-ng-accordion", "closed");
+        item.removeAttribute("open");
       },
     });
   }
